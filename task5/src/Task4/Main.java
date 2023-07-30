03d3f63ad6c6020cd3b9fdd4d9ea8a3e47160c10
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-30 15:11:01 +0200

    Create ArrayList and change elements with ListIterator

--- task5/src/Task4/Main.java
+++ task5/src/Task4/Main.java
@@ -1,4 +1,21 @@
 package Task4;
 
+import java.util.ArrayList;
+import java.util.ListIterator;
+
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            arrayList.add(i);
+        }
+        System.out.println(arrayList);
+        ListIterator listIterator = arrayList.listIterator();
+
+        while (listIterator.hasNext()) {
+            Integer element = (Integer) listIterator.next() + 1;
+            listIterator.set(element);
+        }
+        System.out.println(arrayList);
+    }
 }
