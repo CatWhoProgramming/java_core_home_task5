4b31f150b014941507a881381fbd416a42b232db
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-30 08:26:02 +0200

    Write class DeleteZoo in this class user remove elements from arraylist from class Zoo

--- task5/src/Task3/DeleteZoo.java
+++ task5/src/Task3/DeleteZoo.java
@@ -1,4 +1,19 @@
 package Task3;
 
+import Task2.Zoo;
+
 public class DeleteZoo {
+    public static void main(String[] args) {
+        Zoo zoo = new Zoo();
+        zoo.setAnimals();
+        zoo.getAnimals().remove(3);
+        zoo.getAnimals().remove(5);
+        //Эту команду выполнить невозможно потому
+        // что после удаления двух предыдущих элементов
+        // размер массива уменшается и выдается
+        // ошибка выход за переделы массива
+        //zoo.getAnimals().remove(7);
+        System.out.println(zoo.getAnimals().size());
+
+    }
 }
