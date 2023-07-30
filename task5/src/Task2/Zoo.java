809a692230de71e019f619c25a20f8eb415768c5
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-30 07:32:22 +0200

    Create ArrayList with 8 elements

--- task5/src/Task2/Zoo.java
+++ task5/src/Task2/Zoo.java
@@ -1,4 +1,19 @@
 package Task2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Zoo {
+    public static void main(String[] args) {
+        ArrayList<String> animals = new ArrayList<>();
+        animals.add(0, "cat");
+        animals.add(1, "wolf");
+        animals.add(2, "tiger");
+        animals.add(3, "giraffe");
+        animals.add(4, "monkey");
+        animals.add(5, "elephant");
+        animals.add(6, "lion");
+        animals.add(7, "bear");
+        System.out.println(animals);
+    }
 }
