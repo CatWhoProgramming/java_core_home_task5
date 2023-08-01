fe76db30f10ded7325a5835f397409a627383bab
CatWhoProgramming <kat.servetnik@gmail.com>
2023-08-01 09:11:25 +0200

    Write class Main in Task5 In this class we create object of class Friends and  add and show the list of friends, than sort and also show

--- task5/src/Task5/Main.java
+++ task5/src/Task5/Main.java
@@ -1,4 +1,23 @@
 package Task5;
 
 public class Main {
+    public static void main(String[] args) {
+        Friends friends = new Friends();
+        addFriends(friends);
+        System.out.println(friends.friends);
+        friends.sort();
+        System.out.println(friends.friends);
+    }
+
+    private static void addFriends(Friends friends) {
+        String[] arrayFriends = new String[]{"Kamila", "Leon",
+                "Jasmin", "Lena",
+                "Masha", "Jack", "Denis"};
+        int index;
+        for (int i = 0; i < arrayFriends.length; i++) {
+            index = (int) (Math.random() * (arrayFriends.length));
+            friends.add(arrayFriends[index]);
+
+        }
+    }
 }
