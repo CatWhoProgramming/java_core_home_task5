8a73582518e0e301285e1834ac85ee9479d0a8cd
CatWhoProgramming <kat.servetnik@gmail.com>
2023-08-01 09:26:35 +0200

    Write class Main in Task6 In this class created the list of teachers and shows the best of them

--- task5/src/Task6/Main.java
+++ task5/src/Task6/Main.java
@@ -1,4 +1,26 @@
 package Task6;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> teachers = new ArrayList<>();
+        addTeachers(teachers);
+        System.out.println(teachers);
+        showTheBest(teachers);
+    }
+
+    private static void showTheBest(ArrayList<String> teachers) {
+        System.out.println(teachers.indexOf("Chemistry"));
+    }
+
+    private static void addTeachers(ArrayList<String> teachers) {
+        teachers.add("Mathematics");
+        teachers.add("Chemistry");
+        teachers.add("Literature");
+        teachers.add("English");
+        teachers.add("Physics");
+        teachers.add("History");
+    }
 }
