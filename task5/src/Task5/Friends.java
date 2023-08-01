38360f42b95321fb4237c2e86dc33adfa73f19da
CatWhoProgramming <kat.servetnik@gmail.com>
2023-08-01 09:09:23 +0200

    Write class Friends This class include constructor methods add, show and odject ArrayList

--- task5/src/Task5/Friends.java
+++ task5/src/Task5/Friends.java
@@ -1,4 +1,25 @@
 package Task5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Friends {
+    ArrayList<String> friends;
+
+    Friends() {
+        this.friends = new ArrayList<>();
+    }
+
+    public void add(String value) {
+        this.friends.add(value);
+    }
+
+    public void show() {
+        System.out.println(friends);
+    }
+
+    public void sort() {
+        Collections.sort(this.friends);
+    }
 }
